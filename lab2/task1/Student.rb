2454dd43b346b64b40073235185c7105ce4a3d3b
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-02-26 22:18:42 +0300

    add methods : set contact /and validate

--- lab2/task1/Student.rb
+++ lab2/task1/Student.rb
@@ -2,29 +2,65 @@ class Student
 
 
   PHONE = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+  TELEGRAM = /^@[A-Za-z\d_]{5,32}$/
+  EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  GIT = /\Ahttps:\/\/github\.com\/\w+\z/
 
-  attr_accessor :name, :surname, :lastname, :phone
+  attr_accessor :name, :surname, :lastname, :phone, :telegram, :git, :email
 
   def Student.correct_phone?(str)
-    str.match?([PHONE])
+    str.match?(PHONE)
+  end
+
+  def Student.correct_telegram?(str)
+    str.match?(TELEGRAM)
+  end
+
+  def Student.correct_email?(str)
+    str.match?(EMAIL)
+  end
+
+  def Student.correct_git?(str)
+    str.match?(GIT)
   end
 
   def initialize(arg = {})
     @name = arg[:name]
     @surname = arg[:surname]
     @lastname = arg[:lastname]
-    @phone = arg[:phone]
   end
 
-  def set_phone(phone)
+  def set_contacts(phone, telegram, email, git)
     raise(ArgumentError, 'Not avalible phone') if phone && !Student.correct_phone?(phone)
+    raise(ArgumentError, 'Not avalible telegram') if telegram && !Student.correct_telegram?(telegram)
+    raise(ArgumentError, 'Not avalible email') if email && !Student.correct_email?(email)
+    raise(ArgumentError, 'Not avalible git') if git && !Student.correct_git?(git)
 
     @phone = phone
+    @telegram = telegram
+    @email = email
+    @git = git
+  end
+
+  def validate
+    valid_git
+    valid_other
+  end
+
+  def valid_git
+    raise(ArgumentError,  "You don't write a git id") unless git
+  end
+  def valid_other
+    raise(ArgumentError, 'You must write at list one contact') unless phone || telegram || email
   end
 
   def get_info
-    puts "#{name}, #{surname}, #{lastname}, #{phone}"
+    puts "#{name}, #{surname}, #{lastname}, #{phone}, #{telegram}, #{email}, #{git}"
   end
 
 
 end
+
+obj = Student.new()
+#тут что-то должно быть
+obj.get_info()
