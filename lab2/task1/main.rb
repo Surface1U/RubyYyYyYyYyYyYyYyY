083bd77c3f2725858de6285477246e7691fb57cc
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-02-26 22:31:24 +0300

    Create main.rb

---
+++ lab2/task1/main.rb
@@ -0,0 +1,8 @@
+require_relative 'Student.rb'
+
+test1 = Student.new({name:'Дмитрий', surname:'Джанаев', lastname:'Заурбекович', phone:'+7 962 413 04 87',
+                     telegram:'@Чикиряу)'})
+
+test2 = Student.new({name:'Эмир', surname:'Джанибеков', lastname:'Мунирович',
+                     telegram:'@perdun_zxc)'})
+
