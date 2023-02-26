edf2fc4eeac59304f2faa43af2295b39dd7491b7
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-02-26 21:19:58 +0300

    added check of phone

--- lab2/task1/Student.rb
+++ lab2/task1/Student.rb
@@ -1,19 +1,30 @@
 class Student
-  attr_accessor :name, :surname, :lastname
+
+
+  PHONE = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+
+  attr_accessor :name, :surname, :lastname, :phone
+
+  def Student.correct_phone?(str)
+    str.match?([PHONE])
+  end
 
   def initialize(arg = {})
     @name = arg[:name]
     @surname = arg[:surname]
     @lastname = arg[:lastname]
+    @phone = arg[:phone]
+  end
+
+  def set_phone(phone)
+    raise(ArgumentError, 'Not avalible phone') if phone && !Student.correct_phone?(phone)
+
+    @phone = phone
   end
 
   def get_info
-    puts "#{name}, #{surname}, #{lastname}"
+    puts "#{name}, #{surname}, #{lastname}, #{phone}"
   end
-end
 
-obj = Student.new()
-obj.name = "ssss"
-obj.surname = "sss"
-obj.lastname = "sss"
-obj.get_info()
+
+end
