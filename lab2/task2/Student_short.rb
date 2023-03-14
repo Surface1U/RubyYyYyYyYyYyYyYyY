0c71c1884ef9d12fbb6b3dea1622af63f9851570
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-03-14 16:50:38 +0300

    Update Student_short.rb

--- lab2/task2/Student_short.rb
+++ lab2/task2/Student_short.rb
@@ -2,35 +2,34 @@ require_relative 'Student_super'
 
 class Student_short < Student_super
 
- public_class_method :new
 
-  private
-
-  attr_writer :last_name_and_initials, :contact
-
-  public
-
-  attr_reader :last_name_and_initials, :contact
+   attr_accessor: contact
 
 
   def self.from_student(student)
     raise ArgumentError, 'Student ID is required' if student.id.nil?
 
-    StudentShort.new(student.id, student.short_info)
+    StudentShort.new(student.id, student.get_info, )
   end
   
   
- def initialize(id, info_str)
-    params = JSON.parse(info_str, { symbolize_names: true })
-    raise ArgumentError, 'Fields required: last_name_and_initials' if !params.key?(:last_name_and_initials) || params[:last_name_and_initials].nil?
-
-    self.id = id
-    self.last_name_and_initials = params[:last_name_and_initials]
-    self.contact = params[:contact]
-    self.git = params[:git]
-
+  def StudentShort.from_string(id, str)
+    hash = super(id, str)
+    new(Student.new(hash))
   end
 
+ 
+
+
+  def initialize (params = {id: '', surname: '', git: '', contact: ''})
+    @id = params[:id]
+    @surname = params[:surname]
+    @git = params [:git]
+    @contact = contact
+
+    validate_git
+  end
+ 
   def to_s
     "ID: #{@id}, Surname: #{@surname}, Git: #{@git}, Contact: #{@contact}"
   end
