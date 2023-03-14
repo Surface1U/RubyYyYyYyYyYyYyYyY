b0862c20f2cb6cd4b25ae2099ae5d14e8079ad6f
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-03-14 16:38:57 +0300

    Update Student_short.rb

--- lab2/task2/Student_short.rb
+++ lab2/task2/Student_short.rb
@@ -2,28 +2,34 @@ require_relative 'Student_super'
 
 class Student_short < Student_super
 
-  attr_accessor: contact
+ public_class_method :new
+
+  private
+
+  attr_writer :last_name_and_initials, :contact
+
+  public
+
+  attr_reader :last_name_and_initials, :contact
+
+
+  def self.from_student(student)
+    raise ArgumentError, 'Student ID is required' if student.id.nil?
+
+    StudentShort.new(student.id, student.short_info)
+  end
   
-  def StudentShort.from_string(id, str)
-    hash = super(id, str)
-    new(Student.new(hash))
+  
+ def initialize(id, info_str)
+    params = JSON.parse(info_str, { symbolize_names: true })
+    raise ArgumentError, 'Fields required: last_name_and_initials' if !params.key?(:last_name_and_initials) || params[:last_name_and_initials].nil?
+
+    self.id = id
+    self.last_name_and_initials = params[:last_name_and_initials]
+    self.contact = params[:contact]
+    self.git = params[:git]
+
   end
-
-  attr_reader :id, :full_name,
-              :git, :contacts
-
-  protected :last_name, :first_name, :patronymic,
-            :phone, :telegram, :email,
-            :last_name=, :first_name=, :patronymic=,
-            :phone=, :telegram=, :email=
-
-  def initialize(obj)
-    @id = obj.id
-    @full_name = "#{obj.last_name} #{obj.first_name[0]}.#{obj.patronymic[0]}."
-    @git = obj.git
-    @contacts = "#{obj.phone ? obj.phone : '-'}, #{obj.telegram ? obj.telegram : '-'}, #{obj.email ? obj.email : '-'}"
-  end
- 
 
   def to_s
     "ID: #{@id}, Surname: #{@surname}, Git: #{@git}, Contact: #{@contact}"
