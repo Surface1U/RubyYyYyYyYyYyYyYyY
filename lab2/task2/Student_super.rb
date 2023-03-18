aa086bc6113542fe3bfd57dea206888d2ffe6048
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-03-18 11:16:35 +0300

    Update Student_super.rb

--- lab2/task2/Student_super.rb
+++ lab2/task2/Student_super.rb
@@ -1,35 +1,89 @@
 class Student_super
-  attr_reader :id
-  attr_accessor :surname
-  attr_reader :git
+  #сделали класс абстрактным
+  private_class_method :new
 
-  VALID_GIT = /\Ahttps:\/\/github\.com\/w+\z/
+  protected
 
-  private_class_method: new
+  attr_reader :phone, :email, :telegram
+  attr_writer :id
 
-  def update_git(new_git)
-    @git = new_git
-    validate_git
+  def telegram=(telegram)
+    raise ArgumentError, 'Invalid telegram!' unless telegram.nil? || Student.validate_account?(telegram)
+    @telegram=telegram
   end
 
-  def to_s
-    "ID: #{@id}, Surname: #{@surname}, Git: #{@git}"
+  def git=(git)
+    raise ArgumentError, 'Invalid git!' unless git.nil? || Student.validate_account?(git)
+    @git=git
   end
 
-  def self.is_valid_git?(git)
-    git =~VALID_GIT
+  def email=(email)
+    raise ArgumentError, 'Invalid email!' unless email.nil? || Student.validate_email?(email)
+    @email=email
+  end
+  def phone=(phone)
+    raise ArgumentError, 'Invalid phone number!' unless phone.nil? || Student.validate_phone?(phone)
+    @phone = phone
   end
 
-  def validate_git
-    if @git && !self.is_valid_git?(@git)
-        raise ArgumentError, "Git in wrong format."
+  def set_contacts(email:nil, phone:nil, telegram:nil)
+    self.email = email
+    self.phone = phone
+    self.telegram = telegram
+  end
+
+  public
+
+  attr_reader :id, :git
+  def initialize(id: nil, phone:nil, telegram: nil, email: nil, git:nil )
+    self.id=id
+    set_contacts(**{telegram:telegram, phone: phone, email:email})
+  end
+
+  #проверка на номер
+  def self.validate_phone?(phone)
+    phone.match(/^(8|\+7)[\- ]?\(?\d{3}\)?[\- ]?\d{3}([\- ]?\d{2}){2}$/)
+  end
+
+  #валидация для гита и телеграмма
+  def self.validate_account?(account)
+    account.match(/^@[\w-]+$/)
+  end
+
+  #валидация для почты
+  def self.validate_email?(email)
+    email.match(/^\w+[-\w.]+@([A-Za-z]+\.)+[A-z]{2,4}$/)
+  end
+
+  def git?
+    !git.nil?
+  end
+
+  def exist_contact?
+    !(telegram.nil? && email.nil? && phone.nil?)
+  end
+
+  def validate?
+    git? && exist_contact?
+  end
+
+  #строка для вывода гита
+  def find_git
+    if git?
+      "git=#{git}"
+    else
+      "git: not avalible"
     end
   end
 
-  def validate_git_presence
-    return true if git
-    puts "Git field is required"
-    false
+  #строка для вывода любого одного контакта
+  def find_contact
+    if exist_contact?
+      return "phone: #{phone}" unless phone.nil?
+      return "telegram: #{telegram}" unless telegram.nil?
+      return "email: #{email}" unless email.nil?
+    end
+    "not avalible"
   end
 
 end
