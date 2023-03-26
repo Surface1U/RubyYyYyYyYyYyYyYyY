83f502d934d82c52cd1f710e7fbf9bb14749dc4f
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-03-26 11:29:44 +0300

    Update data_list.rb

--- lab2/task2/data_list.rb
+++ lab2/task2/data_list.rb
@@ -1,23 +1,43 @@
+require_relative 'data_table'
+
 class DataList
-  attr_reader :obj_list
+  
+  private_class_method :new
+  attr_writer :obj_list
+  
   def initialize(obj_list)
     self.obj_list=obj_list
     self.selected_items=[]
   end
 
-  #выделение элементов по номеру
+  #elements with number
   def select(*numbers)
     selected_items.append(*numbers)
   end
-  #массив id выделенных элементов
+
+  #ID_array
   def get_select
     obj_list[selected_items].id
   end
 
   def get_names; end
 
-  def get_data; end
+  #Get table
+  # Template
+  def get_data
+    index_id=0
+    dt = obj_list.inject([]) do |res, object|
+      row=[index_id]
+      row.append(*get_fields(object))
+      index_id+=1
+      res<<row
+    end
+    DataTable.new(dt)
+  end
   protected
-  attr_writer :obj_list
+  attr_reader :obj_list
   attr_accessor :selected_items
+  def get_fields(object)
+    []
+  end
 end
