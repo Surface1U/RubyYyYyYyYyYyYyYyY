b47ae6914ccece8dc3ff14a4291b3d28e166cb01
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-03-27 17:27:18 +0300

    Create data_list_student_short.rb

---
+++ lab2/task2/data_list_student_short.rb
@@ -0,0 +1,21 @@
+require_relative 'data_list'
+require_relative 'data_table'
+
+class DataListStudentShort<DataList
+
+  public_class_method :new
+
+  def initialize(obj_list)
+    super
+  end
+
+  #получение массива наименования аттрибутов
+  def get_names
+    %w[№ short_name git find_contact]
+  end
+
+  protected
+  def get_fields(object)
+    [object.short_name, object.git, object.find_contact]
+  end
+end
