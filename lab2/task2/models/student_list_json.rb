f04b1db8870bde10d77be30b4b7548eb35f15e92
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-04-03 22:15:42 +0300

    Create student_list_json.rb

---
+++ lab2/task2/models/student_list_json.rb
@@ -0,0 +1,16 @@
+require_relative 'strategy'
+require 'json'
+class StudentListJson<StudentListStrategy
+  public_class_method :new
+
+  #получение массива хэшей из строки
+  def list_hash_from_str(str)
+    JSON.parse(str,  {symbolize_names: true })
+  end
+
+  #получение строки заданного вида из массива хэшей
+  def list_hash_to_str(list_hash)
+    JSON.generate(list_hash)
+  end
+
+end
