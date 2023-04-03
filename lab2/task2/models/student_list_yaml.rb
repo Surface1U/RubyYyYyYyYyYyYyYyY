4966b424ea154e5c6a6c7df344a7da6cceedf231
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-04-03 22:16:43 +0300

    Create student_list_yaml.rb

---
+++ lab2/task2/models/student_list_yaml.rb
@@ -0,0 +1,17 @@
+require_relative 'Strategy'
+require 'yaml'
+
+class StudentListYaml<StudentListStrategy
+  public_class_method :new
+
+  #получение массива хэшей из строки
+  def list_hash_from_str(str)
+    YAML.load(str).map {|hash| hash.transform_keys(&:to_sym)}
+  end
+
+  #получение строки заданного вида из массива хэшей
+  def list_hash_to_str(list_hash)
+    list_hash.map{ |hash| hash.transform_keys(&:to_s)}.to_yaml
+  end
+
+end
