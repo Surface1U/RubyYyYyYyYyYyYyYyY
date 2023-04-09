ccb0c482956baa3b7ed22b4eee05b51b6fdd11dc
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-04-09 19:20:03 +0300

    Create dml.sql

---
+++ lab2/task2/db_config/dml.sql
@@ -0,0 +1,4 @@
+insert into students(last_name, first_name, second_name, phone, telegram, email, git) values
+  ('sdfsd', 'asfa', 'sdfdsfgds', '79182331553', NULL, 'NULL', 'NULL'),
+  ('ghghjg', 'dfpghdo', 'xdghiog', NULL, NULL, '@aaaaaaaaaaa@gmail.com', NULL),
+  ('zaaaaasaddasd', 'xho', 'dsgo', '123456789011', '@HORNYCKK', 'sdfphgdsop@mail.ru', '@zhoraKORNI');
