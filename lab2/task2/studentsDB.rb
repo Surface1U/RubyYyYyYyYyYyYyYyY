9d2afe9dbc5764e696d0fa90855d09fc63aa820f
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-04-10 09:11:33 +0300

    Update studentsDB.rb

--- lab2/task2/studentsDB.rb
+++ lab2/task2/studentsDB.rb
@@ -1,6 +1,24 @@
 require 'sqlite3'
+class StudentDB
+  @@instance = nil
+  private def initialize
+    @db = SQLite3::Database.open 'C:\db_student.sql'
+    @db.results_as_hash=true
+  end
 
-db = SQLite3::Database.open "./db_student.sql"
-db.results_as_hash = true
-results = db.execute "SELECT * from students"
-puts results
+  def self.instance
+    if @@instance.nil?
+      @@instance=StudentDB.new
+    end
+    @@instance
+  end
+
+  def execute(query, *args)
+    @db.execute(query,*args)
+  end
+
+  def results_as_hash=(bool)
+    @db.results_as_hash=bool
+  end
+
+end
