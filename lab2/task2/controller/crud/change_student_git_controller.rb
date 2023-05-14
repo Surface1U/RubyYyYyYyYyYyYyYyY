98b3a08396b1fcaf21411834af25138729d3e1ba
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-05-14 11:34:47 +0300

    Add files via upload

---
+++ lab2/task2/controller/crud/change_student_git_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require_relative 'change_student_controller'
+class ChangeStudentGitController<ChangeStudentController
+  public_class_method :new
+  private
+  def get_editable_fields
+    [:git]
+  end
+
+end
---
+++ lab2/task2/controller/crud/change_student_name_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require_relative 'change_student_controller'
+class ChangeStudentNameController<ChangeStudentController
+  public_class_method :new
+  private
+  def get_editable_fields
+    [:first_name, :second_name, :last_name]
+  end
+
+end
